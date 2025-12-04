07aa1ad3230d94ede395fd9ca5ed2ced452f304f
fmarinoa <franco.edson.18014@gmail.com>
2025-12-03 22:56:48 -0500

    feat(vite): add vite-plugin-sitemap for SEO support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -8,7 +9,10 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({ hostname: 'https://el-impostor.francomarino.dev' }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
